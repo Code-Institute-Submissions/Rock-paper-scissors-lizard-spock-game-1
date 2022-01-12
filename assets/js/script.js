d2fabe885ef692383552ecb9ab5046be37c0d46d
kylo74 <kyloc74@gmail.com>
2022-01-12 00:46:20 +0000

    Add code for displaying result when you play

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,11 @@ let playerChoice
 buttons.forEach(button => button.addEventListener('click', (e) => {
     playerChoice = e.target.id
     messages.innerHTML = playerChoice
+    compChoice()
 }))
 
 function compChoice() {
-    let rand = Math.random()
+    const rand = Math.random()
     if (rand <=0.2){
         compChoice = "scizzors"
     } else if (rand <= 0.4){
@@ -25,12 +26,29 @@ function compChoice() {
     }  
 }
 
-function playGame(playerChoice) {  
-
-    let result = checkWinner(choices[compChoice],choices[playerChoice]);
-
-    updateScores(result);
-
-    console.log(playerChoice)
+function result() {  
+    if (compChoice === playerChoice){
+        messages.innerText = "Its a draw"
+    } else if(playerChoice === "Spock" && compChoice === "scizzors"){
+        messages.innerText = "spock beats scizzors you win"
+    } else if(playerChoice === "Spock" && compChoice === "rock"){
+        messages.innerText = "spock beats rock you win"
+    } else if(playerChoice === "Scizzors" && compChoice === "lizard"){
+        messages.innerText = "scizzors beats lizard you win"
+    } else if(playerChoice === "Scizzors" && compChoice === "paper"){
+        messages.innerText = "scizzors beats paper you win"
+    } else if(playerChoice === "Paper" && compChoice === "rock"){
+        messages.innerText = "paper beats rock you win"
+    } else if(playerChoice === "Paper" && compChoice === "spock"){
+        messages.innerText = "paper beats spock you win"
+    } else if(playerChoice === "Rock" && compChoice === "spock"){
+        messages.innerText = "rock beats lizard you win"
+    } else if(playerChoice === "Rock" && compChoice === "scizzors"){
+        messages.innerText = "rock beats scizzors you win"
+    } else if(playerChoice === "Lizard" && compChoice === "spock"){
+        messages.innerText = "lizard beats spock you win"
+    } else if(playerChoice ==="Lizard" && compChoice === "paper" ){
+        messages.innerText = "lizard beats paper you win"
+    }
 }
 
