56e79cd502a2b033ff8ac2b5f347c0e148fb038e
kylo74 <kyloc74@gmail.com>
2022-01-13 11:35:11 +0000

    Fixed name errors in code that determines winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,31 +8,23 @@ let message;
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
     playerChoice = button.id;
-    console.log(playerChoice)
     cpChoice();
-    console.log(compChoice)
     result();
 }));
 
 function cpChoice() {
     const rand = Math.random();
     if (rand <=0.2){
-        console.log('did I get here',rand)
         compChoice = "scizzors";
     } else if (rand <= 0.4){
-        console.log('did I get here',rand)
         compChoice = "paper";
     } else if (rand <= 0.6){
-        console.log('did I get here',rand)
         compChoice = "rock";
     } else if (rand <= 0.8){
-        console.log('did I get here',rand)
         compChoice = "lizard";
     } else {
-        console.log('did I get here',rand)
         compChoice = "spock";
     } 
-    console.log(compChoice)
 }
 
 function result() {  
@@ -50,7 +42,7 @@ function result() {
         message = "paper beats rock you win";
     } else if(playerChoice === "Paper" && compChoice === "spock"){
         message = "paper beats spock you win";
-    } else if(playerChoice === "Rock" && compChoice === "spock"){
+    } else if(playerChoice === "Rock" && compChoice === "lizard"){
         message = "rock beats lizard you win";
     } else if(playerChoice === "Rock" && compChoice === "scizzors"){
         message = "rock beats scizzors you win";
@@ -70,13 +62,13 @@ function result() {
         message = "paper beats rock you lose";
     } else if(compChoice === "paper" && playerChoice === "Spock"){
         message = "paper beats spock you lose";
-    } else if(compChoice === "rock" && playerChoice === "Spock"){
+    } else if(compChoice === "rock" && playerChoice === "Lizard"){
         message = "rock beats lizard you lose";
     } else if(compChoice === "rock" && playerChoice === "Scizzors"){
         message = "rock beats scizzors you lose";
     } else if(compChoice === "lizard" && playerChoice === "Spock"){
         message = "lizard beats spock you lose";
-    } else if(compChoice ==="lizard" && playerChoice === "paper" ){
+    } else if(compChoice ==="lizard" && playerChoice === "Paper" ){
         message = "lizard beats paper you lose";
     }
 
